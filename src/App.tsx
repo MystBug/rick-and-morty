d11e208805b1cac20c16a3420ea5ccbd9677264c
Kiki Gerritsen <kiki.gerritsen@teamrockstars.nl>
2024-05-06 14:28:37 +0200

    use correct typing for search results

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { useState } from "react";
 import { Search } from "./components/search/Search.component";
 import { SearchContext } from "./context/search.context";
 import { Results } from "./components/search/Results.component";
-import { SearchResult } from "./types/search.types";
+import { Character } from "./types/character.types";
 
 function App() {
-  const [searchResults, setSearchResults] = useState<SearchResult[] | null>(
-    null
-  );
+  const [searchResults, setSearchResults] = useState<Character[] | null>(null);
   return (
     <SearchContext.Provider value={{ searchResults, setSearchResults }}>
       <Search />
