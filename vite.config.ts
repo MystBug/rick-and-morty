ec1e3a4b39f1711939f828cac1ea53d44fbba4d6
Kiki Gerritsen <kiki.gerritsen@teamrockstars.nl>
2024-05-08 17:01:43 +0200

    resolve ts errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import react from "@vitejs/plugin-react";
 
 import { dependencies } from "./package.json";
 function renderChunks(deps: Record<string, string>) {
-  const chunks = {};
+  const chunks: Record<string, string[]> = {};
   Object.keys(deps).forEach((key) => {
-    if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+    const containsAtLeasOne = ["react", "react-router-dom", "react-dom"].filter(
+      (item: string) => item === key
+    );
+    if (containsAtLeasOne.length > 0) return;
     chunks[key] = [key];
   });
   return chunks;
